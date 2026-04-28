74a483f6a471cad340ef46574f9b91118a75f058
Betoinn <nico.betoin@gmail.com>
2026-04-28 09:54:05 +0200

    Modif animateur publisher et questions.json
    Modif animateur publisher et questions.json

--- animateur_publisher.py
+++ animateur_publisher.py
@@ -5,7 +5,23 @@ Created on Tue Apr 28 08:41:28 2026
 @author: Utilisateur
 """
 
+import customtkinter as ctk
 import paho.mqtt.client as paho
+import json
+import time
+
+# Configuration interface
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
 
 BROKER = "broker.emqx.io"
 PORT = 1883
+
+TOPIC_ETAT       = f"isen-2026-NBEK/quiz/etat"
+TOPIC_QUESTION   = f"isen-2026-NBEK/quiz/question"
+TOPIC_CORRECTION = f"isen-2026-NBEK/quiz/correction"
+TOPIC_SCORES     = f"isen-2026-NBEK/quiz/scores"
+TOPIC_PRESENCE   = f"isen-2026-NBEK/quiz/presence/+"
+TOPIC_REPONSES   = f"isen-2026-NBEK/quiz/reponse/+"
+TOPIC_RECAP      = f"isen-2026-NBEK/quiz/reponses_recap"
+
